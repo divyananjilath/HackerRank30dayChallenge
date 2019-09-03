f22b9935663cb437c13eb41f2bbc559246542dea
divyananjilath <36256270+divyananjilath@users.noreply.github.com>
2019-09-03 23:13:12 +0530

    Update Day9.c

--- Day9.c
+++ Day9.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int factorial(int n)
+{
+    int f;
+    if(n==0 || n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        f=n*factorial(n-1);
+        return f;
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d",factorial(n));
+    return 0;
+}
 
