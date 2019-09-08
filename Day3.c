488db4474f65405943f6782b2a955633b452bf8d
divyananjilath <36256270+divyananjilath@users.noreply.github.com>
2019-09-08 22:01:13 +0530

    Update Day3.c

--- Day3.c
+++ Day3.c
@@ -1,13 +1,5 @@
-#include <assert.h>
-#include <limits.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 int main()
 {
     int n;
